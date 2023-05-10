2bd479ca454ef689516c7e338715fd292df5e377
Dan <daniel@foresightanalytics.ca>
2023-05-10 17:05:54 -0400

    test client --sender

--- dans-discord-bot-msg.py
+++ dans-discord-bot-msg.py
@@ -6,9 +6,13 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('origin')
 parser.add_argument('channel')
+parser.add_argument('--sender', default='test')
 args = parser.parse_args()
 
 requests.post(
     f'{args.origin}/channel/{args.channel}/message',
-    json={'message': sys.stdin.read()},
+    json={
+        'message': sys.stdin.read(),
+        'sender': args.sender,
+    },
 )
